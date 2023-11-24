d9b30acbd5f697645ea9345078f42dedc648cddd
Alekshamon <aleksandrahamon@gmail.com>
2023-11-24 12:02:19 +0100

    Premier composant

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,20 @@
+function PokemonCard () {
+
+    return (
+
+    <div>
+        <figure>
+            <img     
+                src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+                alt="Bulbasaur" 
+                className="Image"/>
+         
+        </figure>
+        <figcaption> 
+        <h1>Bulbasaur</h1>
+        </figcaption>
+    </div> 
+    );
+}
+
+export default PokemonCard; 
